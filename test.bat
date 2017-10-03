5e8d754ba347eccd88cf5c102d9025b9ca9a5e95
Dave E Collett (DELWP) <dave.collett@delwp.vic.gov.au>
2017-10-04 10:02:11 +1100

    test.bat

--- test.bat
+++
@@ -1,2 +0,0 @@
-python rinex_preprocess.py -f 35802630.T01 --ninefig 290100240 -a 1.382 -c TRM51943 -o DaveCollett
-python rinex_preprocess.py -f 35802631.T01 --ninefig 307700210 -a 1.382 -c TRM51943 -o DaveCollett
